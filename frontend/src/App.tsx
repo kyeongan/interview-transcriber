f14a690f02ac3dd4ca5cab3ad1b0d27f8ee0ccdb
Karl Kwon <newmkka@gmail.com>
2025-09-27 17:57:33 -0400

    Fix TypeScript build error and simplify Vercel config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import Header from './components/Header';
 import FileUploader from './components/FileUploader';
